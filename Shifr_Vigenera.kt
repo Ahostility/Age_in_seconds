3609d26e0870a349ea3126f3fdd6dac8b5373b9d
Ahostility <ale1431999@gmail.com>
2019-06-16 16:01:56 +0300

    Update

--- Shifr_Vigenera.kt
+++ Shifr_Vigenera.kt
@@ -1,5 +1,6 @@
 import kotlin.math.abs
-fun shifr_Vigenera(our_text:String,abc:Array<CharArray>,key:String):String{
+
+fun shifr_Vigenera(our_text:String,abc:Array<CharArray>,key:String):String {
     var shifrotext:String = ""
    // var arrlist = Array(26, {CharArray(26)})
     var newkey:String = ""
@@ -28,6 +29,7 @@ fun shifr_Vigenera(our_text:String,abc:Array<CharArray>,key:String):String{
     }
     return shifrotext
 }
+
 fun decoding_Vigener(shifr:String,abc:Array<CharArray>,key:String):String{
     var our_text:String = ""
     var newkey:String = ""
@@ -56,6 +58,7 @@ fun decoding_Vigener(shifr:String,abc:Array<CharArray>,key:String):String{
     }
     return our_text
 }
+
 fun shifr_Boufora(our_text:String,abc:Array<CharArray>,key:String):String{
     var shifrotext:String = ""
     // var arrlist = Array(26, {CharArray(26)})
@@ -84,6 +87,7 @@ fun shifr_Boufora(our_text:String,abc:Array<CharArray>,key:String):String{
     }
     return shifrotext
 }
+
 fun decooding_Boufora(shifr:String,abc:Array<CharArray>,key:String):String{
     var our_text:String = ""
     var newkey:String = ""
@@ -112,47 +116,90 @@ fun decooding_Boufora(shifr:String,abc:Array<CharArray>,key:String):String{
     }
     return our_text
 }
+
+fun index(abc:ArrayList<Char>, our_text:String):Double{
+    var count:Double = 0.0
+    val countlist = arrayListOf<Double>()
+    var indc:Double = 0.0
+    for(i in 0..abc.size-1) {
+        for(j in 0..our_text.length-1) {
+            if(abc[i] == our_text[j]){
+                count+=1.0
+            }
+        }
+//        println("${countlist[i]}, ${abc[i]}")
+//        indc+=countlist[i]*(countlist[i]-1)/(our_text.length*(our_text.length-1))//True 0.062 ~ 0.067
+        countlist.add(count)
+        indc+=count*(count-1)/(our_text.length*(our_text.length-1))//True 0.062 ~ 0.067
+        count = 0.0
+    }
+//    println(indc)
+    return indc
+}
+
+fun listik4(our_text:String,key:String){
+    var abclist = arrayListOf<Char>()
+//    var textPart = Array(key.length,{CharArray()})
+    for(i in 0..key.length-1){
+        for(j in i..our_text.length-1 step key.length){
+            print(our_text[j])
+            abclist.add(our_text[j])
+        }
+        println(abclist[i])
+        var textPart = Array(key.length,{CharArray(abclist.size)})
+       // textPart[i] = abclist
+    }
+}
+
 fun main(){
+    //val ind = 0.067
     val ABC = arrayListOf<Char>()//Create abc on ASCII
     val abclist = Array(26,{CharArray(26)})//Tabula Vigenera
     for(i in 65..90){
         ABC.add("$i".toByte().toChar())
     }
+    print("Our abc is: ")
     println(ABC)
-    for(i in 0..abclist.size - 1){
+
+    for(i in 0..abclist.size - 1) {
         for (j in 0..abclist.size - 1) {
             abclist[i][j] = ABC[(i + j) % ABC.size]
         }
+        println(abclist[i])
     }
-//    val our_text = readLine()!!.toString()
-    val origin:String = "HELLOMYFRIEND"
+
+    val origin:String = "BEINGYOURSLAVEWHATSHOULDIDOBUTTENDUPONTHEHOURSANDTIMESOFYOURDESIRE" +
+            "IHAVENOPRECIOUSTIMEATALLTOSPENDNORSERVICESTODOTILLYOUR" +
+            "EQUIRENORDAREICHIDETHEWORLDWITHOUTENDHOURWHILSTIMYSOVERE" +
+            "IGNWATCHTHECLOCKFORYOUNORTHINKTHEBITTERNESSOFABSENCESOURWH" +
+            "ENYOUHAVEBIDYOURSERVANTONCEADIEUNORDAREIQUESTIONWITHMYJEALOUS" +
+            "THOUGHTWHEREYOUMAYBEORYOURAFFAIRSSUPPOSEBUTLIKEASADSLAVESTAYA" +
+            "NDTHINKOFNOUGHTSAVEWHEREYOUAREHOWHAPPYYOUMAKETHOSESOT" +
+            "RUEAFOOLISLOVETHATINYOURWILLTHOUGHYOUDOANYTHINGHETHINKSNOILL"
     val our_key:String = "KEY"
-    //RIJVSKIJPSILN
-    println("Shifr_Vigenera")
-    println(shifr_Vigenera(origin,abclist,our_key))
-    println(decoding_Vigener(shifr_Vigenera(origin,abclist,our_key),abclist,our_key))
+
+
+//    println("Shifr_Vigenera")
+//    println(shifr_Vigenera(origin,abclist,our_key))
+//    println(decoding_Vigener(shifr_Vigenera(origin,abclist,our_key),abclist,our_key))
     println("-----------------------------------------------------------------------")
-    println("The Boufort cipher")
-    println(shifr_Boufora(origin,abclist,our_key))
-    println(decooding_Boufora(shifr_Boufora(origin,abclist,our_key),abclist,our_key))
+//    println("The Boufort cipher")
+//    println(shifr_Boufora(origin,abclist,our_key))
+//    println(decooding_Boufora(shifr_Boufora(origin,abclist,our_key),abclist,our_key))
 
 
 
 
     //Breacking Veginer
     val shifrotext = shifr_Vigenera(origin,abclist,our_key)
-    val lenstring = shifrotext.length
-    val lenkey = readLine()!!.toInt()
-    val ic = 0.055
-    println("${shifrotext}, $lenstring, $lenkey")
-        //f[i]*(f[i]-1)
+    val lenshifr = shifrotext.length
+    //val lenkey = readLine()!!.toInt()
 
-
-
-
-
-
-
+    println("len our text ${origin.length}")
+    print("Index C: ")
+    println(index(ABC,origin))
+    listik4(origin,our_key)
+//    println("${shifrotext}, $lenshifr, $lenkey")
 
 
 
